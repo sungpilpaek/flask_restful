9d44d6795c5528ebd47aa70fa788d3f758d89bfc
Sung-Pil Paek <sungpil0228@gmail.com>
2017-06-30 19:20:57 +0900

    Modified setup.py and deleted test.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
     packages=find_packages(),
     install_requires=[
-            'flask>=0.10.1',
-            'flask_restful>=0.3.6',
-            'pycrypto>=2.6'
-        ],
+        'flask>=0.10.1',
+        'flask_restful>=0.3.6',
+        'pycrypto>=2.6'
+    ],
     setup_requires=[
-            'pytest-runner'
-        ],
+        'pytest-runner'
+    ],
     tests_require=[
-            'pytest'
-        ],
+        'pytest'
+    ],
 )
--- tests/database_context_manager_test.py
+++ tests/database_context_manager_test.py
@@ -4,3 +4,24 @@ from example_server.database import context_manager
 
 class TestContextManager(object):
     def 
+
+    # import requests
+    # from flask import Flask, request, url_for
+    # import json
+
+
+    # index = ''
+    # while True:
+    #     res = json.loads(requests.get("http://localhost:5000/api/v1/subscribers", {'index': index}).json())
+    #     index = res["index"]
+    #     print res
+    #     if res["data"] == []:
+    #         break
+
+    # app = Flask(__name__)
+    # with app.test_request_context():
+    #     print url_for('postsubscribers')
+
+    # app = Flask(__name__)
+    # with app.test_request_context('/api/v1/subscibers', method='POST'):
+    #     print request.path

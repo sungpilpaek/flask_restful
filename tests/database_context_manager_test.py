63270207fcc1b02487a88360720283eec1a8c741
Sung-Pil Paek <sungpil0228@gmail.com>
2017-07-01 11:30:28 +0900

    Refactoed test codes to use fixtures in conftest.py

--- tests/database_context_manager_test.py
+++ tests/database_context_manager_test.py
@@ -1,4 +1,3 @@
-import os
 import pytest
 import sqlite3
 from database import context_manager
@@ -6,18 +5,15 @@ from common import exception, config
 
 
 class TestContextManager(object):
-    def create_tmp_db(self):
-        config.SQLITE_PATH = ":memory:"
-
-    def test_integrityerror(self):
-        self.create_tmp_db()
+    def test_integrityerror(self, tmp_db):
+        config.SQLITE_PATH = tmp_db
         with pytest.raises(exception.InvalidUsername):
             db_instance = context_manager.DatabaseContextManager()
             with db_instance:
                 raise sqlite3.IntegrityError()
 
-    def test_query(self):
-        self.create_tmp_db()
+    def test_query(self, tmp_db):
+        config.SQLITE_PATH = tmp_db
         db_instance = context_manager.DatabaseContextManager()
         with db_instance:
             cur = db_instance.query(

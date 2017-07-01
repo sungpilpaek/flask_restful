a6eb3c77a803e5e2588698d8f4f167a647aa86e4
Sung-Pil Paek <sungpil0228@gmail.com>
2017-07-01 11:28:15 +0900

    Deleted unnecessary comments!

--- tests/database_context_manager_test.py
+++ tests/database_context_manager_test.py
@@ -25,25 +25,3 @@ class TestContextManager(object):
             )
             res = str(cur.fetchone()["TO_PLAY_PS4"])
             assert res == "What a beautiful day!"
-
-
-# import requests
-# from flask import Flask, request, url_for
-# import json
-
-
-# index = ''
-# while True:
-#     res = json.loads(requests.get("http://localhost:5000/api/v1/subscribers", {'index': index}).json())
-#     index = res["index"]
-#     print res
-#     if res["data"] == []:
-#         break
-
-# app = Flask(__name__)
-# with app.test_request_context():
-#     print url_for('postsubscribers')
-
-# app = Flask(__name__)
-# with app.test_request_context('/api/v1/subscibers', method='POST'):
-#     print request.path

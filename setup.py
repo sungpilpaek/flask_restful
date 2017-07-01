f43824cd6c415cfa395d5b55f1c02412d3e3991d
Sung-Pil Paek <sungpil0228@gmail.com>
2017-07-01 11:38:10 +0900

    Deleted setup.cfg and modified setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,10 @@ setup(
     install_requires=[
         'flask>=0.10.1',
         'flask_restful>=0.3.6',
-        'pycrypto>=2.6'
+        'pycrypto>=2.6',
+        'pytest'
     ],
     setup_requires=[
         'pytest-runner'
-    ],
-    tests_require=[
-        'pytest'
-    ],
+    ]
 )
